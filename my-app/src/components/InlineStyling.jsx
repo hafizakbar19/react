754cbe9e4052d65a4e082bbc4bf64be65a966844
hafizakbar19 <hafizakbar19@gmail.com>
2025-09-17 13:40:34 +0500

    dynamic styling

--- my-app/src/components/InlineStyling.jsx
+++ my-app/src/components/InlineStyling.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const InlineStyling = () => {
+    const [userCard, setUserCard] =useState({
+        border: '2px solid gray',
+            width: '200px',
+            textAlign: 'center',
+            borderRadius: '10px',
+            margin: '15px',
+            padding: '10px'
+    })
+
+    const [grid,setGrid] = useState(true);
+    const setTheme = (bgColor,textColor,borderColor) => {
+        setUserCard({...userCard,backgroundColor:bgColor,color:textColor,borderColor:borderColor});
+    }
+
   return (
     <>
         <h1>Inline styling</h1>
+        <button onClick={()=>setTheme("red","white","black")}>Red theme</button>
+        <button onClick={()=>setTheme("","","gray")}>default theme</button>
+        <button onClick={()=>setGrid(!grid)}>{grid?"List view":"Grid view"}</button>
         <div style={{
-            display: 'flex',
+            display: grid ? 'flex' : 'block',
             flexWrap: 'wrap'
         }}>
-        <div style={{
-            border: '2px solid gray',
-            width: '200px',
-            textAlign: 'center',
-            borderRadius: '10px',
-            margin: '15px',
-            padding: '10px'
-        }}>
+        <div style={userCard}>
             <img style={{
                 width: '200px',
                 borderTopRightRadius: '10px',
@@ -24,14 +34,7 @@ const InlineStyling = () => {
             <h2>Sajid Ali</h2>
             <h4>Web developer</h4>
         </div>
-        <div style={{
-            border: '2px solid gray',
-            width: '200px',
-            textAlign: 'center',
-            borderRadius: '10px',
-            margin: '15px',
-            padding: '10px'
-        }}>
+        <div style={userCard}>
             <img style={{
                 width: '200px',
                 borderTopRightRadius: '10px',
@@ -40,14 +43,7 @@ const InlineStyling = () => {
             <h2>Sajid Ali</h2>
             <h4>Web developer</h4>
         </div>
-        <div style={{
-            border: '2px solid gray',
-            width: '200px',
-            textAlign: 'center',
-            borderRadius: '10px',
-            margin: '15px',
-            padding: '10px'
-        }}>
+        <div style={userCard}>
             <img style={{
                 width: '200px',
                 borderTopRightRadius: '10px',
@@ -56,14 +52,7 @@ const InlineStyling = () => {
             <h2>Sajid Ali</h2>
             <h4>Web developer</h4>
         </div>
-        <div style={{
-            border: '2px solid gray',
-            width: '200px',
-            textAlign: 'center',
-            borderRadius: '10px',
-            margin: '15px',
-            padding: '10px'
-        }}>
+        <div style={userCard}>
             <img style={{
                 width: '200px',
                 borderTopRightRadius: '10px',
@@ -72,14 +61,7 @@ const InlineStyling = () => {
             <h2>Sajid Ali</h2>
             <h4>Web developer</h4>
         </div>
-        <div style={{
-            border: '2px solid gray',
-            width: '200px',
-            textAlign: 'center',
-            borderRadius: '10px',
-            margin: '15px',
-            padding: '10px'
-        }}>
+        <div style={userCard}>
             <img style={{
                 width: '200px',
                 borderTopRightRadius: '10px',
@@ -88,14 +70,7 @@ const InlineStyling = () => {
             <h2>Sajid Ali</h2>
             <h4>Web developer</h4>
         </div>
-        <div style={{
-            border: '2px solid gray',
-            width: '200px',
-            textAlign: 'center',
-            borderRadius: '10px',
-            margin: '15px',
-            padding: '10px'
-        }}>
+        <div style={userCard}>
             <img style={{
                 width: '200px',
                 borderTopRightRadius: '10px',
@@ -104,14 +79,7 @@ const InlineStyling = () => {
             <h2>Sajid Ali</h2>
             <h4>Web developer</h4>
         </div>
-        <div style={{
-            border: '2px solid gray',
-            width: '200px',
-            textAlign: 'center',
-            borderRadius: '10px',
-            margin: '15px',
-            padding: '10px'
-        }}>
+        <div style={userCard}>
             <img style={{
                 width: '200px',
                 borderTopRightRadius: '10px',
@@ -120,14 +88,7 @@ const InlineStyling = () => {
             <h2>Sajid Ali</h2>
             <h4>Web developer</h4>
         </div>
-        <div style={{
-            border: '2px solid gray',
-            width: '200px',
-            textAlign: 'center',
-            borderRadius: '10px',
-            margin: '15px',
-            padding: '10px'
-        }}>
+        <div style={userCard}>
             <img style={{
                 width: '200px',
                 borderTopRightRadius: '10px',
@@ -136,14 +97,7 @@ const InlineStyling = () => {
             <h2>Sajid Ali</h2>
             <h4>Web developer</h4>
         </div>
-        <div style={{
-            border: '2px solid gray',
-            width: '200px',
-            textAlign: 'center',
-            borderRadius: '10px',
-            margin: '15px',
-            padding: '10px'
-        }}>
+        <div style={userCard}>
             <img style={{
                 width: '200px',
                 borderTopRightRadius: '10px',
@@ -152,14 +106,7 @@ const InlineStyling = () => {
             <h2>Sajid Ali</h2>
             <h4>Web developer</h4>
         </div>
-        <div style={{
-            border: '2px solid gray',
-            width: '200px',
-            textAlign: 'center',
-            borderRadius: '10px',
-            margin: '15px',
-            padding: '10px'
-        }}>
+        <div style={userCard}>
             <img style={{
                 width: '200px',
                 borderTopRightRadius: '10px',
@@ -168,14 +115,16 @@ const InlineStyling = () => {
             <h2>Sajid Ali</h2>
             <h4>Web developer</h4>
         </div>
-        <div style={{
-            border: '2px solid gray',
-            width: '200px',
-            textAlign: 'center',
-            borderRadius: '10px',
-            margin: '15px',
-            padding: '10px'
-        }}>
+        <div style={userCard}>
+            <img style={{
+                width: '200px',
+                borderTopRightRadius: '10px',
+                borderTopLeftRadius: '10px'
+            }} src='https://plus.unsplash.com/premium_photo-1664536392779-049ba8fde933?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D' alt='user image'/>
+            <h2>Sajid Ali</h2>
+            <h4>Web developer</h4>
+        </div>
+        <div style={userCard}>
             <img style={{
                 width: '200px',
                 borderTopRightRadius: '10px',
