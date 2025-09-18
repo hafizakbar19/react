725b8939d3d30bfc88170d4dce08d8c571cacc4a
hafizakbar19 <hafizakbar19@gmail.com>
2025-09-18 14:45:21 +0500

    useRef hook practice

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,5 @@
-import Bootstrap from "./components/bootstrap/Bootstrap";
+
+import UseRefHook from "./components/UseRefHook";
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
         <h1 style={{ color: "chocolate" }}>React Practice</h1>
       </div>
       <p className="read-the-docs">By Mohammed Akbar Ali</p>
-      <Bootstrap />
+      <UseRefHook />
     </>
   );
 }
---
+++ my-app/src/components/UseRefHook.jsx
@@ -0,0 +1,33 @@
+import React, { useRef, useState, useEffect } from "react";
+
+const UseRefHook = () => {
+  const h1ref = useRef();
+  const [col, setCol] = useState(true);
+
+  const changeColor = () => {
+    col
+      ? (h1ref.current.style.color = "red")
+      : (h1ref.current.style.color = "yellow");
+    setCol(!col);
+  };
+
+  const startTimeRef = useRef(Date.now()); // initial value is current time
+  const [now, setNow] = useState(Date.now());
+
+  useEffect(() => {
+    const id = setInterval(() => setNow(Date.now()), 1000);
+    return () => clearInterval(id);
+  }, []);
+
+  const seconds = Math.floor((now - startTimeRef.current) / 1000);
+
+  return (
+    <div>
+      <h1 ref={h1ref}>Use Ref hook practice in React</h1>
+      <button onClick={changeColor}>Change color</button>
+      <p>Seconds passed: {seconds}</p>;
+    </div>
+  );
+};
+
+export default UseRefHook;
