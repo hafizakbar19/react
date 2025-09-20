9a67cd489c28e22407874aee86585227a6a4b100
hafizakbar19 <hafizakbar19@gmail.com>
2025-09-20 22:16:04 +0500

    js

--- jsPlayGround/script.js
+++ jsPlayGround/script.js
@@ -13,12 +13,69 @@
 // }
 // console.log(strSize)
 
-const person = {
-    name: "Akbar",
-    age: 35,
-    city: "Karachi"
-}
+// const person = {
+//     name: "Akbar",
+//     age: 35,
+//     city: "Karachi"
+// }
 
-for(let detail in person){
-    console.log(detail, ":" , person[detail])
-}
+// for(let detail in person){
+//     console.log(detail, ":" , person[detail])
+// }
+
+// string length
+
+// let myStr = prompt("enter your string");
+// let strLen = myStr.length;
+
+// for(let i = strLen; i >= 0; i--){
+//     console.log(myStr[i])
+// }
+
+//string methods
+// myStr.includes(".") ? console.log('it contains dot') : console.log('it does not contain dot');
+// myStr.startsWith("Pak") ? console.log('it starts with Pak') : console.log('it does not starts with Pak');
+// myStr.endsWith(".") ? console.log('it ends with dot') : console.log('it does not ends with dot');
+// let sliced = myStr.slice(2,5); // last index in excluded
+// let replaced = myStr.replaceAll('a','e');
+
+//assignment password generator.
+
+// let pwd = myStr.slice(0,3).concat('#');
+// let combined = pwd.concat(myStr.length);
+// console.log(combined);
+
+// let sum = 0;
+//  for (let i of myArr) {
+//     console.log(i);
+//     sum+=i;
+// }
+// console.log(sum)
+
+// for (let i = myArr.length; i >= 0; i--){
+//     console.log(myArr[i]);
+// }
+// let myArr = [2, 4, 5, 4, 8, 19];
+// let greaterNumber = myArr[0];
+// for (let i = 0; i <= myArr.length; i++) {
+//   if(myArr[i] > greaterNumber){
+//     greaterNumber = myArr[i];
+//   }
+// }
+// console.log(greaterNumber);
+
+// let count = 0;
+// for (let i = 0; i <= myArr.length; i++) {
+//   if (myArr[i] % 2 == 0) count++;
+// }
+// console.log(count);
+
+
+let fruits = ["Apple", "Banana", "Mango", "Orange"];
+console.log(fruits)
+
+// let spliced = fruits.splice(2,1,"Cherry");
+// console.log(fruits)
+
+let myStr = "Akbar Ali"
+
